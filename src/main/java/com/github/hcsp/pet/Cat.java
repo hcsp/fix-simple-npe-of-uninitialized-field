a06f31851d309b1e6d132af1a2206b3c23110b6d
frank-0908 <2059326740@qq.com>
2019-07-12 21:57:56 +0800

    修复一个空指针异常

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -5,8 +5,12 @@ public class Cat {
 
     /** Return the name length of this cat. Return zero if it's anonymous. 返回这只猫名字的长度。如果没有名字则返回0。 */
     public int getNameLength() {
+
         // Fix the NullPointerException thrown in this method
         // 在本方法中，修复抛出的空指针异常（NullPointerException）
+        if (name == null){
+            return 0;
+        }
         return name.length();
     }
 }
