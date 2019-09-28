f267dfd57d2ba084766db3063792f593ec658d33
ChenXinghe <55908722+XingheChen@users.noreply.github.com>
2019-09-28 19:50:07 +0800

    修复一个空指针异常使用try...catch (#89)
    修复一个空指针异常使用try...catch (#89)

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -7,6 +7,13 @@ public class Cat {
     public int getNameLength() {
         // Fix the NullPointerException thrown in this method
         // 在本方法中，修复抛出的空指针异常（NullPointerException）
-        return name.length();
+        int result = 0;
+        try {
+            result = name.length();
+        }
+        catch (NullPointerException e) {
+            result = -1;
+        }
+        return result;
     }
 }
