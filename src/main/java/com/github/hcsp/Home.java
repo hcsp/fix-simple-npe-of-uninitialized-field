db797ea312536a4505b85d624407c89ffdcc53d6
HP <591639554@qq.com>
2020-04-10 10:11:40 +0800

    修复空指针异常

--- src/main/java/com/github/hcsp/Home.java
+++ src/main/java/com/github/hcsp/Home.java
@@ -3,22 +3,33 @@ package com.github.hcsp;
 import com.github.hcsp.pet.Cat;
 
 public class Home {
+
     public static void main(String[] args) {
+
         Cat tom = new Cat();
+
         tom.name = "Tom";
+
         // Tom's name length is 3
+
         // 名叫Tom的猫名字的长度是3个字母
+
         System.out.println("name length: " + tom.getNameLength());
 
+
+
         // An anonymous cat's name length should be -1, but we get an exception here
+
         // Try to fix the bug in Cat.getNameLength() method
+
         // 一只没有名字的猫的名字长度本来应该是-1，但是现在抛出了一个异常
+
         // 请在Cat.getNameLength()方法中修复此问题
+
         Cat anonymousCat = new Cat();
-        if(anonymousCat.name == null){
-            System.out.println("name为空");
-        }else {
-            System.out.println("name length: " + anonymousCat.getNameLength());
-        }
+
+        System.out.println("name length: " + anonymousCat.getNameLength());
+
     }
+
 }
