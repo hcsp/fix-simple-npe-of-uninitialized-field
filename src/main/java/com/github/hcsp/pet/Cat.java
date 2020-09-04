d1bcdf71b12d2947672be2fa2552de5e4ee556c7
Faiz <53805895+faiz-lab@users.noreply.github.com>
2020-09-04 21:47:20 +0800

    修复空指针异常 (#238)
    修复空指针异常 (#238)

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -7,6 +7,10 @@ public class Cat {
     public int getNameLength() {
         // Fix the NullPointerException thrown in this method
         // 在本方法中，修复抛出的空指针异常（NullPointerException）
-        return name.length();
+        if (name == null) {
+            return -1;
+        } else {
+            return name.length();
+        }
     }
 }
