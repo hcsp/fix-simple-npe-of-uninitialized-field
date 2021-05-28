c0a95455124d999d14196fc76ec3e1a588f5d09c
bowen-wu <33862944+bowen-wu@users.noreply.github.com>
2021-05-28 14:57:20 +0800

    feat: judge null pointer (#273)
    feat: judge null pointer (#273)

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -3,10 +3,15 @@ package com.github.hcsp.pet;
 public class Cat {
     public String name;
 
-    /** Return the name length of this cat. Return -1 if it's anonymous. 返回这只猫名字的长度。如果没有名字则返回-1。 */
+    /**
+     * Return the name length of this cat. Return -1 if it's anonymous. 返回这只猫名字的长度。如果没有名字则返回-1。
+     */
     public int getNameLength() {
         // Fix the NullPointerException thrown in this method
         // 在本方法中，修复抛出的空指针异常（NullPointerException）
-        return name.length();
+        if (name != null) {
+            return name.length();
+        }
+        return -1;
     }
 }
