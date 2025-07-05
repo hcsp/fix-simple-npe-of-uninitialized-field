9835017293b31a8b117f71cd05d87f5a507f4154
lyra <lyraaaaaaaaaaa@gmail.com>
2025-07-06 09:36:53 +1000

    I've fixed it.

--- src/main/java/com/github/hcsp/pet/Cat.java
+++ src/main/java/com/github/hcsp/pet/Cat.java
@@ -1,12 +1,13 @@
 package com.github.hcsp.pet;
 
-public class Cat {
-    public String name;
+public class Cat {         // Cat class
+    public String name;     // field
 
     /** Return the name length of this cat. Return -1 if it's anonymous. 返回这只猫名字的长度。如果没有名字则返回-1。 */
     public int getNameLength() {
-        // Fix the NullPointerException thrown in this method
-        // 在本方法中，修复抛出的空指针异常（NullPointerException）
-        return name.length();
+        if (name == null) {
+            return -1;
+        } else
+            return name.length();
     }
 }
